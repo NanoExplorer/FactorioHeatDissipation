02247aa211bd7a5d1b3bff8a3352cfdb9c607226
Christopher Rooney <christopher@rooneyworks.com>
2026-05-23 22:25:19 -0600

    Add metadata

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend {
     name = "HeatDissipation-kw-per-tile",
     type = "double-setting",
     setting_type = "startup",
-    default_value = 20,
+    default_value = 40,
     minimum_value = 0,
   },
 }
